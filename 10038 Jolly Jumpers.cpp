5e8b85cf223a60f6b8f4e4c8f4c0217ec5d07e36
Muhammad Idzhar Darissalam <idzharbae@gmail.com>
2018-04-20 15:45:03 +0700

    Update 10038 Jolly Jumpers.cpp
    fuck bitset dude just use array lmao

--- 10038 Jolly Jumpers.cpp
+++ 10038 Jolly Jumpers.cpp
@@ -1,22 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main(){
-	int n;
+	int n, arr[3001];
 	while(cin>>n){
-		int bit = 0, arr[n];
-		for(int i = 0; i < n; i++){
+		int mark[3001] = {0};
+		for(int i = 0; i < n ; i++){
 			cin>>arr[i];
-			int diff = abs(arr[i] - arr[i-1])-1;
-			if(i > 0 and diff >= 0 and diff < n) bit |= ( 1 << diff );
+			if(i > 0){
+				int diff = abs(arr[i] - arr[i-1]) -1 ;
+				if(diff >= 0 and diff < n)
+					mark[diff] = 1;
+			}
 		}
-		bit++;
-		int x = (bit>> (n-1) );
-		x = (x << (n-1) );
-		if(bit == x) cout<<"Jolly\n"; else cout<<"Not jolly\n";
-		bitset<32> y(bit);
-		//cout<<bit<<' '<<x<<endl;
-		//cout<<y<<endl;
+		int flag = 1;
+		for(int i = 0; i < n-1; i++){
+			if(!mark[i]){
+				cout<<"Not jolly\n";
+				flag = 0;
+				break;
+			}
+		}
+		if(flag)
+		cout<<"Jolly\n";
 	}
 return 0;
 }
