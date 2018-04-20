da4df31139c4682515a29a6347c88937fcbd7e5c
Muhammad Idzhar Darissalam <idzharbae@gmail.com>
2018-04-20 15:21:50 +0700

    Update 10038 Jolly Jumpers.cpp
    edited diff, must be less than n.
    Previously we only check if variable "bit" is a power of 2,

--- 10038 Jolly Jumpers.cpp
+++ 10038 Jolly Jumpers.cpp
@@ -8,11 +8,11 @@ int main(){
 		for(int i = 0; i < n; i++){
 			cin>>arr[i];
 			int diff = abs(arr[i] - arr[i-1])-1;
-			if(i > 0 and diff >= 0) bit |= ( 1 << diff );
+			if(i > 0 and diff >= 0 and diff < n) bit |= ( 1 << diff );
 		}
 		bit++;
-		int x = (bit>> (int)(log(bit)/log(2)) );
-		x = (x << (int)(log(bit)/log(2)) );
+		int x = (bit>> (n-1) );
+		x = (x << (n-1) );
 		if(bit == x) cout<<"Jolly\n"; else cout<<"Not jolly\n";
 		bitset<32> y(bit);
 		//cout<<bit<<' '<<x<<endl;
